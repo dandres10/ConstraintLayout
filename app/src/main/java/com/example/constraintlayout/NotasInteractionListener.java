396f07a8c1dbdbc65cc30d2dd4c5b2f6c6480bc0
Andrés León León <marlon.leon@infotrack.com.co>
2020-01-10 16:00:16 -0500

    curso seccion 5 calse 07

---
+++ app/src/main/java/com/example/constraintlayout/NotasInteractionListener.java
@@ -0,0 +1,9 @@
+package com.example.constraintlayout;
+
+public interface NotasInteractionListener {
+
+    void editNotaClik(Nota nota);
+    void eliminarNotaClick(Nota nota);
+    void favoritaNotaClick(Nota nota);
+
+}
