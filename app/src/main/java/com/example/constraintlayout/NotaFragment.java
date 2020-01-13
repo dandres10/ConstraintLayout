7551e1b2f282b667fd427e6aa2089f9d34af3969
Andrés León León <dandresleon64@gmail.com>
2020-01-12 21:49:30 -0500

    orientacion del dispositivo

--- app/src/main/java/com/example/constraintlayout/NotaFragment.java
+++ app/src/main/java/com/example/constraintlayout/NotaFragment.java
@@ -9,13 +9,13 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
 import java.util.List;
-
 
 
 public class NotaFragment extends Fragment {
@@ -27,7 +27,6 @@ public class NotaFragment extends Fragment {
     private NotasInteractionListener mListener;
     private List<Nota> notaList;
     private MyNotaRecyclerViewAdapter adapterNotas;
-
 
 
     public NotaFragment() {
@@ -61,16 +60,21 @@ public class NotaFragment extends Fragment {
         if (view instanceof RecyclerView) {
             Context context = view.getContext();
             RecyclerView recyclerView = (RecyclerView) view;
-            if (mColumnCount <= 1) {
+
+            if (DefinirOrientacion(view)) {
                 recyclerView.setLayoutManager(new LinearLayoutManager(context));
             } else {
-                recyclerView.setLayoutManager(new StaggeredGridLayoutManager( mColumnCount,StaggeredGridLayoutManager.VERTICAL));
+
+                recyclerView.setLayoutManager(new StaggeredGridLayoutManager(GenerarColumnas(context), StaggeredGridLayoutManager.VERTICAL));
             }
 
             notaList = new ArrayList<>();
-            notaList.add(new Nota("Lista de la compra" , "comprar pan tostado ", true,android.R.color.holo_blue_light));
-            notaList.add(new Nota("Recordar","deje el carro en la empresa que formare en un futuro",false,android.R.color.holo_green_light));
-            notaList.add(new Nota("Cumpleaños" , "no olvidar la velas ", true,android.R.color.holo_orange_light));
+            notaList.add(new Nota("Lista de la compra", "comprar pan tostado ", true, android.R.color.holo_blue_light));
+            notaList.add(new Nota("Recordar", "deje el carro en la empresa que formare en un futuro ", false, android.R.color.holo_green_light));
+            notaList.add(new Nota("Cumpleaños", "no olvidar la velas ", true, android.R.color.holo_orange_light));
+            notaList.add(new Nota("Planes de salir", "ir a un lugar que tenga una casa cerca al mar ", true, android.R.color.holo_blue_light));
+            notaList.add(new Nota("Cumpleaños de tesoro", "guardar para el regalo", false, android.R.color.holo_green_light));
+            notaList.add(new Nota("formar una empresa", "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.", true, android.R.color.holo_orange_light));
             adapterNotas = new MyNotaRecyclerViewAdapter(notaList, mListener);
             recyclerView.setAdapter(adapterNotas);
         }
@@ -95,6 +99,16 @@ public class NotaFragment extends Fragment {
         mListener = null;
     }
 
+
+    private int GenerarColumnas(Context context) {
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        return (int) ((displayMetrics.widthPixels / displayMetrics.density) / 180);
+    }
+
+
+    private boolean DefinirOrientacion(View view) {
+        return view.getId() == R.id.listPortrait ? true : false;
+    }
 
 
 }
